194db681f9e26be0a7e584ac83bbc26777a377b2
KIUA XLL <kiuaxv@gmail.com>
2026-07-08 10:26:31 +1400

    Create Color.kt

---
+++ app/src/main/java/com/x/launcher/ui/theme/Color.kt
@@ -0,0 +1,29 @@
+package com.x.launcher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Core Palette
+val XBlack = Color(0xFF000000)
+val XDarkGray = Color(0xFF1A1A1A)
+val XCardDark = Color(0xFF111111)
+
+// Butter Yellow / Cream
+val XButterYellow = Color(0xFFFFF6B0)
+val XButterLight = Color(0xFFFFF8C4)
+val XButterDeep = Color(0xFFFFE881)
+
+// Accents
+val XWhite = Color(0xFFFFFFFF)
+val XGrayLight = Color(0xFFB0B0B0)
+val XGrayDark = Color(0xFF555555)
+val XTeal = Color(0xFF6FBF9F)
+
+// Buttons
+val XButtonBlack = Color(0xFF0A0A0A)
+val XButtonHover = Color(0xFF2A2A2A)
+
+// Status
+val XGreen = Color(0xFF4CAF50)
+val XRed = Color(0xFFE53935)
+val XYellow = Color(0xFFFFC107)
+val XBlue = Color(0xFF42A5F5)
